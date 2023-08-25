4d3f15e4f97777ef9699352fb00a69fd5cb52eee
pan1c <>
2023-08-25 18:56:23 +0100

    Validation added

--- app/modules/map_generator.py
+++ app/modules/map_generator.py
@@ -2,10 +2,14 @@ import pgeocode
 import csv
 import os
 import logging
+import math
 
 def get_coordinates_by_postcode(country_code, postcode):
     geolocator = pgeocode.Nominatim(country_code)
     location = geolocator.query_postal_code(postcode)
+    if math.isnan(location.latitude) or math.isnan(location.longitude):
+        logging.warning(f"Unable to find coordinates for country code {country_code} and postcode {postcode}")
+        return None
     return location.latitude, location.longitude
 
 def generate_map_html():
@@ -27,10 +31,11 @@ def generate_map_html():
             data.extend([(row['country'], row['postcode'], row['label']) for row in reader])
         logging.debug(f"Read {len(data)} locations from {csv_path}")
 
-    locations = [
-        (get_coordinates_by_postcode(country, postcode), label)
-        for country, postcode, label in data
-    ]
+    locations = []
+    for country, postcode, label in data:
+        coords = get_coordinates_by_postcode(country, postcode)
+        if coords:  # Check if the coordinates are not None
+            locations.append((coords, label))
 
     # Generate markers
     markers = ""
